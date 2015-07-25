571a39bdba1aa6a733092a4d2bfb0448e2eaace3
MohammadFathi <m.fathi08@hotmail.com>
2015-07-24 21:50:42 -0700

    Create UniqueWord.cpp

---
+++ src/UniqueWord.cpp
@@ -0,0 +1,51 @@
+
+
+#include "UniqueWord.h"
+#include<iostream>
+#include <string>
+using namespace std;
+
+Unique_Word::Unique_Word() {
+	 WordString="";
+	 NumberOfRepeating=0;
+	 LastTweetNumber=0;
+
+}
+void Unique_Word::UpdateNumberOfRepeating(){
+		NumberOfRepeating++;
+}
+
+void Unique_Word::UpdateLastTweetNumber(long x){
+		LastTweetNumber=x;
+
+}
+
+
+void Unique_Word::UpdateWordString(string s){
+	 	 WordString=s;
+}
+
+
+long Unique_Word::GetNumberOfRepeating(){
+		return NumberOfRepeating;
+}
+
+
+
+long Unique_Word::GetLastTweetNumber(){
+	 	 return LastTweetNumber;
+}
+
+
+
+string Unique_Word::GetWordString(){
+	 	 return WordString;
+}
+
+
+
+
+Unique_Word::~Unique_Word() {
+	// TODO Auto-generated destructor stub
+}
+
