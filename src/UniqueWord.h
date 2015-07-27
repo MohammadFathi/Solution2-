0ae1b59424f495f21fd3b4cef5ecbfa820cf6afd
MohammadFathi <m.fathi08@hotmail.com>
2015-07-27 00:52:00 -0700

    Update UniqueWord.h

--- src/UniqueWord.h
+++ src/UniqueWord.h
@@ -9,20 +9,57 @@ using namespace std;
 
 
 class Unique_Word {
-	string WordString;				// the word string itself.
+	string WordString;			// the word string itself.
 	long NumberOfRepeating;			// How many times were this word repeated in the whole tweets.txt file up to now.
-	long LastTweetNumber;		// Last tweet number we found this word during our analysis, if not found before it will be initially zero.
+	long LastTweetNumber;		        // Last tweet number we found this word during our analysis, if not found before it will be initially zero.
 
 public:
-	Unique_Word();
-	void UpdateNumberOfRepeating();
-	void UpdateLastTweetNumber(long);
-	void UpdateWordString(string );
-	long GetNumberOfRepeating();
-	long GetLastTweetNumber();
-	string GetWordString();
-	void Clean();
-	~Unique_Word();
+
+	Unique_Word() {
+		 WordString="";
+		 NumberOfRepeating=0;
+		 LastTweetNumber=0;
+	}
+	
+	void UpdateNumberOfRepeating(){
+			NumberOfRepeating++;
+	}
+
+	void UpdateLastTweetNumber(long x){
+			LastTweetNumber=x;
+
+	}
+
+
+	void UpdateWordString(string s){
+		 	 WordString=s;
+	}
+
+
+	long GetNumberOfRepeating(){
+			return NumberOfRepeating;
+	}
+
+
+
+	long GetLastTweetNumber(){
+		 	 return LastTweetNumber;
+	}
+
+
+
+	string GetWordString(){
+		 	 return WordString;
+	}
+
+
+	void Clean(){
+			WordString="";
+			NumberOfRepeating=0;
+			LastTweetNumber=0;
+
+	}
+	~Unique_Word(){}
 };
 
 #endif /* UNIQUEWORD_H_ */
